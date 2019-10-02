b2d92ee89890414356a4f913752c2d4fec492744
Rory Wagstaff <rfwagstaff@gmail.com>
2019-10-02 20:41:52 +0100

    Add files via upload

---
+++ Game.Classes/GameContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Game.Classes
+{
+    public class GameContext
+    {
+    }
+}
---
+++ Game.Classes/UserInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessTheNumber.Classes
+{
+    public class UserInput
+    {
+        public UserInput(string input)
+        {
+            Input = input;
+        }
+
+        public string Input { get; set; }
+    }
+}

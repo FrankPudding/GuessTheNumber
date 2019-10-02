84736c96d8fb849e46346ba9a3764e4a355d878f
Rory Wagstaff <rfwagstaff@gmail.com>
2019-10-02 20:53:47 +0100

    Add files via upload

---
+++ GuessTheNumberConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using GuessTheNumber.Classes;
+using GuessTheNumber.Externals;
+
+namespace GuessTheNumberConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var userInputReader = new UserInputReader();
+            var messageWriter = new MessageWriter();
+            var messageProvider = new MessageProvider();
+            var guessValidator = new GuessValidator();
+            var guessEvaluator = new GuessEvaluator();
+
+            var game = new Game(userInputReader, messageWriter, messageProvider, guessValidator, guessEvaluator);
+
+            game.Play();
+        }
+    }
+}

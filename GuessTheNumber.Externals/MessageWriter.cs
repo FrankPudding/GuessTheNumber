0a0ca2a16ebf9cf3633c3d86841a0023a01a545c
Rory Wagstaff <rfwagstaff@gmail.com>
2019-10-02 20:51:14 +0100

    Add files via upload

---
+++ GuessTheNumber.Externals/MessageWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using GuessTheNumber.Contracts;
+
+namespace GuessTheNumber.Externals
+{
+    public class MessageWriter : IMessageWriter
+    {
+        public void WriteMessage(string message, ConsoleColor? colour = null)
+        {
+            if(colour.HasValue)
+            {
+                Console.ForegroundColor = colour.Value;
+            }
+            else
+            {
+                Console.ResetColor();
+            }
+
+            Console.WriteLine(message);
+
+            Console.ResetColor();
+        }
+    }
+}
---
+++ GuessTheNumber.Externals/UserInputReader.cs
@@ -0,0 +1,18 @@
+﻿using GuessTheNumber.Contracts;
+using System;
+
+namespace GuessTheNumber.Externals
+{
+    public class UserInputReader : IUserInputReader
+    {
+        public string UserInput()
+        {
+            return Console.ReadLine();
+        }
+
+        public string UserInputKey()
+        {
+            return Console.ReadKey().KeyChar.ToString();
+        }
+    }
+}

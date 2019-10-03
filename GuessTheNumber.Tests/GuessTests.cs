916ab9fa43d4baba82e4ef7e55d3a57275ad7457
Rory Wagstaff <rfwagstaff@gmail.com>
2019-10-03 12:46:00 +0100

    Added more parameterized tests

--- GuessTheNumber.Tests/GuessTests.cs
+++ GuessTheNumber.Tests/GuessTests.cs
@@ -7,35 +7,67 @@ namespace GuessTheNumber.Tests
     {
         [Theory]
         [InlineData("1", 1)]
+        [InlineData("1.0", 1)]
         [InlineData("2", 2)]
+        [InlineData("3", 3)]
+        [InlineData("4", 4)]
+        [InlineData("5", 5)]
+        [InlineData("6", 6)]
+        [InlineData("7", 7)]
+        [InlineData("8", 8)]
+        [InlineData("9", 9)]
+        [InlineData("10", 10)]
         public void GuessValidatorTestPositive(string inputString, int inputInt)
         {
             var guessValidator = new GuessValidator();
 
             guessValidator.TryValidate(inputString, out int result);
 
-            Assert.True(result == inputInt);
+            Assert.Equal(inputInt, result);
+        }
+
+        [Theory]
+        [InlineData("-1")]
+        [InlineData("0")]
+        [InlineData("1.1")]
+        [InlineData("1001")]
+        [InlineData("")]
+        [InlineData("a")]
+        [InlineData("b")]
+        public void GuessValidatorTestNegative(string inputString)
+        {
+            var guessValidator = new GuessValidator();
+
+            Assert.False(guessValidator.TryValidate(inputString, out int result));
+            Assert.Equal(0, result);
         }
 
         [Theory]
         [InlineData(1)]
-        public void GuessEvaluatorTest1(int actualTarget)
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        [InlineData(10)]
+        public void GuessEvaluatorTest(int target)
         {
-            var guess = new Guess(actualTarget, actualTarget, 0);
+            var guess = new Guess(target, target, 0);
             var guessEvaluator = new GuessEvaluator();
 
-            guess.StartGame(actualTarget);
-
-            Assert.Equal(5, 5);
+            Assert.True(guessEvaluator.IsCorrect(guess.GuessNumber, guess.Target));
         }
 
-        // tests for guessevaluator and guessvalidator
+        // tests for guessevaluator and guessvalidator - done
 
-        // make interfaces for writers/readers - done?
+        // make interfaces for writers/readers - done
 
         // mocking (interfaces)??
 
-        // parameterize - done?
+        // parameterize - done
 
         // resharper/code coverage
     }
